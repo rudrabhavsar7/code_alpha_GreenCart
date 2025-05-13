88ace1f1c925cc394dd9183ad94ce47dbd87afdf
rudrabhavsar7 <23dcs012@charusat.edu.in>
2025-05-13 15:53:56 +0530

    fix changes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -15,7 +15,7 @@ import Cart from './pages/Cart'
 import AddAddress from './pages/AddAddress'
 import MyOrders from './pages/MyOrders'
 import SellerLogin from './components/seller/SellerLogin'
-import SellerLayout from './pages/seller/sellerLayout'
+import sellerLayout from './pages/seller/sellerLayout'
 import AddProduct from './pages/seller/AddProduct'
 import ProductList from './pages/seller/ProductList'
 import Orders from './pages/seller/Orders'
@@ -40,7 +40,7 @@ const App = () => {
           <Route path="/add-address" element={<AddAddress/>} />
           <Route path="/my-orders" element={<MyOrders/>} />
           <Route path="/loader" element={<Loading/>} />
-          <Route path="/seller" element={isSeller ? <SellerLayout/> : <SellerLogin/>}>
+          <Route path="/seller" element={isSeller ? <sellerLayout/> : <SellerLogin/>}>
             <Route index element={isSeller ? <AddProduct/> : null}/>
             <Route path="product-list" element={<ProductList/>}/>
             <Route path="orders" element={<Orders/>}/>
