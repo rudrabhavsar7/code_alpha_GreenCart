b895df29c5a0bada88fcdf438894e65e052281db
rudrabhavsar7 <23dcs012@charusat.edu.in>
2025-05-13 15:59:57 +0530

    syntax

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -40,7 +40,7 @@ const App = () => {
           <Route path="/add-address" element={<AddAddress/>} />
           <Route path="/my-orders" element={<MyOrders/>} />
           <Route path="/loader" element={<Loading/>} />
-          <Route path="/seller" element={isSeller ? <sellerLayout/> : <SellerLogin/>}>
+          <Route path="/seller" element={isSeller ? <SellerLayout/> : <SellerLogin/>}>
             <Route index element={isSeller ? <AddProduct/> : null}/>
             <Route path="product-list" element={<ProductList/>}/>
             <Route path="orders" element={<Orders/>}/>
